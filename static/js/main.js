71b07a72992f65ff6be443fd74d31719299b36b2
zmy <mengyu.chou@gmail.com>
2014-02-23 19:27:42 +0800

    more detailed description

--- static/js/main.js
+++ static/js/main.js
@@ -3,20 +3,23 @@ jQuery(function($) {
         help: function() {
             this.echo("info         Show information about the Hackthon");
             this.echo("register     Register LabmU's Hackthon");
+            this.echo("labmu        Link to Lab mU homepage");
+            this.echo("");
         },
         info: function() {
-            this.echo("Time:");
-            this.echo("Place:");
-            this.echo("Theme:");
-            this.echo("Description:");
+            this.echo("Time: Sat 2014-03-01 10AM~10PM +0800");
+            this.echo("Place: TBA");
+            this.echo("Theme: Mobile development on TUNet application (http://tunet.lab.mu).");
+            this.echo("Best if U: Had exp in Android, iOS development or mobile UX design.");
+            this.echo("");
         },
         register: function() {
             var term = this,
                 register_info = {};
             term
                 .push(function(command){
-                    if(command.match(/y|yes/i)){
-                        term.echo('sending your register info...');
+                    if(command.match(/Y|y|yes|Yes/i)){
+                        term.echo('Sending your register info...');
                         $.ajax({
                             type:'POST',
                             headers:{'x-csrf-token':$('meta[name="csrf"]').attr('content')},
@@ -24,38 +27,45 @@ jQuery(function($) {
                             success:function(res){},
                             error:function(res){}
                         });
+                        term.echo('We will contact you later :D');
+                        term.echo("");
                         term.pop();
-                    }else if (command.match(/n|no/i)){
+                    }else if (command.match(/N|n|no|No/i)){
                         term.echo('cancelled.');
+                        term.echo("");
                         term.pop();
                     }
                 },{
                     prompt:'Is your register info correct?(y/n)'
                 })
                 .push(function(command){
-                    register_info["github"] = command;
+                    register_info["Github"] = command;
                     term.pop();
                 },{
-                    prompt: 'Your Github Id:'
+                    prompt: 'Your Github Id: '
                 })
                 .push(function(command) {
                     register_info["Email"] = command;
                     term.pop();
                 }, {
-                    prompt: 'Your Email:'
+                    prompt: 'Your Email: '
                 })
                 .push(function(command) {
                     register_info["Phone"] = command
                     term.pop();
                 }, {
-                    prompt: 'Your Phone Number:'
+                    prompt: 'Your Phone Number: '
                 })
                 .push(function(command) {
                     register_info["Name"] = command;
                     term.pop();
                 }, {
-                    prompt: 'Your Name:'
+                    prompt: 'Your Name: '
                 });
+        },
+        labmu: function() {
+            this.echo("Time: Sat 2014-03-01 10:00:00 +0800");
+            this.echo("");
         }
     }, {
         greetings: function() {
@@ -66,7 +76,8 @@ jQuery(function($) {
                 "|  '--.\\ '-'  || `-' ||  |  |  |'  '-'  '    |  |  |  |\\ '-'  |\\ `--.|  \\  \\  |  |  |  | |  |' '-' '|  ||  | \n" +
                 "`-----' `--`--' `---' `--`--`--' `-----'     `--'  `--' `--`--' `---'`--'`--' `--'  `--' `--' `---' `--''--' \n" +
                 "                                                                                                             \n" +
-                "type help for help";
+                "What can I do to [[u]help] you?
+                ";
         }
     });
 });
