c4be49521ff400c644ce2d0b6756a49017ebb405
cgcgbcbc <cgcgbcbc@163.com>
2014-02-23 14:39:17 +0800

    write register info with time stamp

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var views = require('koa-views');
 var serve = require('koa-static');
 var csrf = require('koa-csrf');
 var session = require('koa-session');
+var fs = require('fs');
 var app = koa();
 app.keys=['secret'];
 csrf(app);
@@ -21,4 +22,43 @@ app.use(route.get('/', function * (next) {
     this.body = yield this.render('index', {csrf:this.csrf});
 }));
 
+app.use(route.post('/',function * (next){
+    var body = yield parse(this),
+        name = body.Name,
+        phone = body.Phone,
+        email = body.Email,
+        github = body.github,
+        response = this;
+
+    try{
+        this.assertCSRF(body);
+    } catch (err){
+        this.status = 403;
+        this.body = {
+            message: 'invalid request'
+        };
+    }
+
+    console.log(body);
+    try{
+        yield write(name+'\t'+phone+'\t'+email+'\t'+github);
+    }catch(err){
+        response.status = 403;
+        response.body = {
+            message: 'invalid request'
+        };
+    }
+    response.status = 200;
+    response.body = {
+        message: 'ok'
+    };
+
+}));
+
 app.listen(3001);
+
+function write(data){
+    return function(done){
+        fs.writeFile('data/'+Date.now(),data,done);
+    }
+}
